72f390af3451e9f726c2b62a28a43b4e0fc9faf4
Aaron Belovsky <ambelovsky@gmail.com>
2019-04-25 22:38:46 -0500

    Dependencies set to update

--- scripts/getdeps.sh
+++ scripts/getdeps.sh
@@ -1,3 +1,3 @@
-go get "github.com/ambelovsky/gosf"
-go get "github.com/ambelovsky/gosf-sample-plugin"
-go get "github.com/ambelovsky/gosf-message-counter"
+go get -u "github.com/ambelovsky/gosf"
+go get -u "github.com/ambelovsky/gosf-sample-plugin"
+go get -u "github.com/ambelovsky/gosf-message-counter"
