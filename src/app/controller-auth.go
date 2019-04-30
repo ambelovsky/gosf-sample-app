7d3875af056b004967080ecf50020c41ab8bed5e
Aaron Belovsky <ambelovsky@gmail.com>
2019-04-30 11:07:37 -0500

    Added broadcast example.

--- src/app/controller-auth.go
+++ src/app/controller-auth.go
@@ -1,6 +1,9 @@
 package main
 
-import f "github.com/ambelovsky/gosf"
+import (
+	"github.com/ambelovsky/gosf"
+	f "github.com/ambelovsky/gosf"
+)
 
 // Auth contains all auth.* endpoint controllers
 type Auth struct{}
@@ -10,6 +13,11 @@ func (controller Auth) Register(client *f.Client, request *f.Request) *f.Message
 	response := new(f.Message)
 	response.Success = true
 	response.Text = "client registered"
+
+	adminMessage := new(f.Message)
+	adminMessage.Success = true
+	adminMessage.Text = "new client registered"
+	gosf.Broadcast("admin", "admin.register", adminMessage)
 
 	return response
 }
