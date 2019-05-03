ccfc7054311ae4f9af650bd2d44add592cd12ec2
Aaron Belovsky <aaron.belovsky@riverkidstexas.com>
2019-05-02 22:51:09 -0500

    Updated with sample body argument parsing

--- src/app/controller-util.go
+++ src/app/controller-util.go
@@ -7,9 +7,21 @@ type Util struct{}
 
 // Echo returns the passed message back to the client
 func (controller Util) Echo(client *f.Client, request *f.Request) *f.Message {
+	var description string
+
+	// Argument Parsing
+	if val, ok := request.Message.Body["description"]; ok {
+		description = val.(string)
+	}
+
 	response := new(f.Message)
 	response.Success = true
 	response.Text = PluginSamplePlugin.Echo(request.Message.Text)
+
+	// If a detailed description was entered, send it back to the client
+	if description != "" {
+		response.Text += " - " + description
+	}
 
 	return response
 }
